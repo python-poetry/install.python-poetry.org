0741abd8905fbc5d88cea390fca10e560d473b9d
caorantaoyao <caoran.taoyao@gmail.com>
2026-08-08 23:33:52 +0800

    fix: Installation fails on MacOS

--- install-poetry.py
+++ install-poetry.py
@@ -327,7 +327,7 @@ class VirtualEnvironment:
             import ensurepip  # noqa: F401
             import venv
 
-            builder = venv.EnvBuilder(clear=True, with_pip=True, symlinks=False)
+            builder = venv.EnvBuilder(clear=True, with_pip=True, symlinks=MACOS)
             context = builder.ensure_directories(target)
 
             if (
